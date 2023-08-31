bd6859aa4ad0814d19da8fd854d12a2386dc447c
SinekaV <sneehefty@gmail.com>
2023-08-31 13:09:10 +0530

    version controlling changes need to be done

--- config/index.go
+++ config/index.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"time"
-     "grpcmodel/constants"
+	"github.com/SinekaV/grpc-constants/constants"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
